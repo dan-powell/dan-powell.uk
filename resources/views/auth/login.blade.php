35d9c193f3e5c048b8b551693224ed434edcf64f
Dan Powell <dan@autumnrain.co.uk>
2015-07-20 23:01:17 +0100

    Styled login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,59 @@
 @extends('base')
 
+@section('meta')
+<title>Login | Dan Powell</title>
+<meta name="robots" content="noindex">
+@stop
+
+@section('class')Auth _login @stop
+
 @section('main')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="/auth/login">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-									Login
-								</button>
-
-								<a href="/password/email">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
+    <section class="Section -hero -primary -lg">
+	    <div class="_container -center">
+            @if (count($errors) > 0)
+				<div class="alert alert-danger">
+					<strong>Whoops!</strong> There were some problems with your input.<br><br>
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
 				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+			@endif
+
+			<form class="form" role="form" method="POST" action="{{ url() }}/auth/login">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<div class="form-group">
+					<label class="control-label">Username</label>
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+				</div>
+
+				<div class="form-group">
+					<label class="control-label">Password</label>
+                    <input type="password" class="form-control" name="password">
+				</div>
+
+				<div class="form-group">
+				    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember"> Remember Me
+                        </label>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary">
+						Login
+					</button>
+				</div>
+			</form>
+	    </div>
+    </section>
+
+@stop
+
+@section('sidebar')
+
+@stop
