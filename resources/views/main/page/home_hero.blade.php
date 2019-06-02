3081ceac609f20c17a70cd3854cea8ca6cd740bc
Dan Powell <dan@dan-powell.uk>
2019-06-02 22:35:15 +0100

    Re-jigged the blog again

---
+++ resources/views/main/page/home_hero.blade.php
@@ -0,0 +1,280 @@
+<svg class="HomeHero-bg Svg" viewBox="0 0 830 720">
+    <defs>
+        <style>
+            .Svg g {
+                transform-origin: center;
+                fill-rule: evenodd;
+            }
+            .Svg tspan {
+                font-family: Caveat;
+                font-size: 36px;
+                font-weight: normal;
+                line-spacing: 1;
+                fill: rgba(255,255,255,.8);
+            }
+            .Svg .arrow {
+                stroke: rgba(255,255,255,.6);
+                stroke-width: 4;
+                stroke-linecap: round;
+                stroke-linejoin: round;
+            }
+            .Svg .box {
+                stroke: rgba(255,255,255,.6);
+                fill: none;
+                stroke-width: 3;
+            }
+            .Svg .line {
+                stroke: rgba(255,255,255,.9);
+                stroke-width: 3;
+            }
+            .Svg .line.med {stroke-width: 6;}
+            .Svg .line.thick {stroke-width: 15;}
+            .Svg .line.wide {stroke-width: 20;}
+            .Svg .line.dash {
+                stroke-linecap: round;
+                stroke-linejoin: round;
+                stroke-dasharray: 12;
+            }
+        </style>
+    </defs>
+    <g class="HomeHeroBg-one" fill="none" fill-rule="evenodd" opacity="0">
+        <g class="HomeHeroBg-boxes">
+            <g>
+                <path class="box" d="M181.5 33h57v57h-57z" />
+            </g>
+            <g>
+                <path class="box" d="M180.5 103h57v567h-57z" />
+            </g>
+            <g>
+                <path class="box" d="M250.5 593h437v77h-437z" />
+            </g>
+            <g>
+                <path class="box" d="M251.5 193h437v117h-437z" />
+            </g>
+            <g>
+                <path class="box" d="M551.5 323h137v117h-137z" />
+            </g>
+            <g>
+                <path class="box" d="M251.5 461h137v117h-137z" />
+            </g>
+            <g>
+                <path class="box" d="M401.5 393.5h137v117h-137z" />
+            </g>
+            <g>
+                <path class="box" d="M251.5 33.5h197v147h-197z" />
+            </g>
+            <g>
+                <path class="box" d="M461.5 33.5h227v147h-227z" />
+            </g>
+        </g>
+        <g class="HomeHeroBg-icons">
+            <path class="line" d="M191.5 113.5h37v37h-37z" />
+            <path class="line med" d="M197.5 51.5H222M197.5 61.5H222M197.5 72.5H222" />
+        </g>
+        <g class="HomeHeroBg-content">
+            <path class="line thick" d="M301 363.5h180M301 334.5h180" />
+            <path class="line thick" d="M189 651h40M189 626h40M189 600h40"/>
+            <path class="line thick" d="M485 107h180M485 78h180M485 136h180" />
+        </g>
+        <g class="HomeHeroBg-labels">
+            <g>
+                <path d="M164.33 62.8l-10.1-3.32 6.5 17.88 6.22-13.5c-21.17 8.98-36.93 10.24-47.81 5.08" class="arrow"/>
+                <text transform="rotate(19 33.1 117.23)">
+                    <tspan x="9" y="47">burger</tspan>
+                    <tspan x="18" y="82">menu</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M164.8 364.78l-7.9 5.96 1.65-18.95 8.51 11.3c-21.78-3.07-37.47.78-46.5 7.6" class="arrow"/>
+                <text transform="rotate(19 -952.8 447.6)">
+                    <tspan x="3" y="71">sidebar</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M711.03 632.1l7.62-7.41 2 18.92-10.52-9.46c22.97-1.2 36.64-7.93 44.21-16.34" class="arrow"/>
+                <text transform="rotate(19 -1297.6 2432.4)">
+                    <tspan x="14" y="46">footer</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M553.67 472.66l5.18 9.28 5.9-18.17-11.85 7.7c21.52 4.57 34.17 12.38 41.1 23.05" class="arrow"/>
+                <text transform="rotate(-18 1792.32 -1436.78)">
+                    <tspan x="8" y="69">portfolio </tspan>
+                    <tspan x="25" y="104">items</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M165.03 141.13l-3.38 9.3-9.85-16.38 13.3 4.84c-19.95 9.3-30.52 19.75-34.87 31.7" class="arrow"/>
+                <text fill="#000" transform="translate(79 129)">
+                    <tspan x="9.11" y="76">home</tspan>
+                    <tspan x="0" y="111">button</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M161.19 631.5l-2.57-9.56-11.23 15.45 14.53-3.16c-19.92-11.5-29.54-22.83-32.84-35.12" class="arrow"/>
+                <text transform="rotate(-18 1671.57 52.92)">
+                    <tspan x="20" y="44">site</tspan>
+                    <tspan x="9" y="79">name</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M715.03 99.1l7.62-7.41 2 18.92-10.52-9.46c22.97-1.2 36.64-7.93 44.21-16.34" class="arrow"/>
+                <text transform="rotate(19 338.28 2154.94)">
+                    <tspan x="2" y="51">lead text</tspan>
+                </text>
+            </g>
+            <g>
+                <text transform="rotate(19 311.1 163.23)">
+                    <tspan x="269" y="110">profile pic</tspan>
+                </text>
+            </g>
+            <g>
+                <text transform="rotate(-18 466.41 170.05)">
+                    <tspan x="400" y="235">hero</tspan>
+                    <tspan x="361" y="270">illustration</tspan>
+                </text>
+            </g>
+        </g>
+    </g>
+    <g class="HomeHeroBg-two" fill="none" fill-rule="evenodd" opacity="0">
+        <g class="HomeHeroBg-boxes">
+            <g>
+                <path class="box" d="M183 194h504v394H183z"/>
+            </g>
+            <g transform="translate(180 461)">
+                <path class="box" d="M1.5 1.5h118v127H1.5z"/>
+            </g>
+            <g transform="translate(180 461)">
+                <path class="box" stroke-width="3" d="M191.5 51.5h317v77h-317z"/>
+            </g>
+        </g>
+        <g class="HomeHeroBg-labels">
+            <g>
+                <path d="M264.55 359.67l5.22-9.14-18.76 3.03 12.23 8.01c-5.56-22.29-3.56-37.4 2.3-47.71" class="arrow"/>
+                <text transform="rotate(19 -637.56 879.02)">
+                    <tspan x="3.71" y="45.93">player</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M452.15 484.35l7.22-7.67 2.59 18.82-11.24-9.35c22.94-1.2 36.81-7.53 44.95-16.15" class="arrow"/>
+                <text transform="rotate(19 -980.8 1584.55)">
+                    <tspan x="15.1" y="47">enemy</tspan>
+                </text>
+            </g>
+            <g>
+                <text transform="rotate(19 45.12 966.12)">
+                    <tspan x="145.17" y="317.16">jump</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M239.36 178.14l5.07-9.24-18.7 3.36 12.36 7.8c-5.95-22.2-4.21-37.34 1.47-47.75" class="arrow"/>
+                <text transform="translate(210 89)">
+                    <tspan x="0" y="35.74">score</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M613.41 418.85l1.42-10.44-16.25 9.84 14.33 2.85c-13.5-18.58-17.31-33.35-15.73-45.1" class="arrow"/>
+                <text transform="rotate(-18 1323.69 -1537.88)">
+                    <tspan x="3.3" y="47.43">pickups</tspan>
+                </text>
+            </g>
+            <g>
+                <path d="M653.44 178.26l-1.06-10.48 15.9 10.4-14.42 2.34c14.14-18.1 18.46-32.72 17.3-44.52" class="arrow"/>
+                <text transform="rotate(19 127.02 1965.55)">
+                    <tspan x="3.57" y="46.74">health</tspan>
+                </text>
+            </g>
+        </g>
+        <g class="HomeHeroBg-icons">
+            <g transform="translate(240 375)">
+                <path class="line" d="M20 41.35L37.57 76.5H2.43L20 41.35z"/>
+            </g>
+            <g transform="translate(240 375)">
+                <circle class="line" cx="20" cy="19" r="17.5"/>
+            </g>
+            <g>
+                <path class="line med dash" d="M302.58 425.8c15.74-15.73 55.62-31.54 80.91-15.73 16.46 10.29 30.42 27.42 37.42 54.2"/>
+            </g>
+            <g transform="translate(190 194)">
+                <path class="line" d="M440.03 265.53l1.68-9.76.13-.78-.57-.55-7.1-6.92 9.81-1.42.79-.12.34-.7 4.39-8.9 4.39 8.9.34.7.79.12 9.8 1.42-7.1 6.92-.56.55.13.78 1.68 9.76-8.77-4.6-.7-.37-.7.36-8.77 4.61z"/>
+            </g>
+            <g transform="translate(190 194)">
+                <path class="line" d="M235.14 279l17.14 12.45-6.55 20.15h-21.18L218 291.45z"/>
+            </g>
+            <g>
+                <path class="line" d="M659.29 233.15s-13.88-12.53-14.27-18.38c-.4-5.84 6.07-13.83 14.27-4.47 8.34-9.36 14.8-1.37 14.42 4.47-.4 5.85-14.42 18.38-14.42 18.38z"/>
+                <path class="line" d="M622.29 233.15s-13.88-12.53-14.27-18.38c-.4-5.84 6.07-13.83 14.27-4.47 8.34-9.36 14.8-1.37 14.42 4.47-.4 5.85-14.42 18.38-14.42 18.38z"/>
+                <path class="line" d="M585.29 233.15s-13.88-12.53-14.27-18.38c-.4-5.84 6.07-13.83 14.27-4.47 8.34-9.36 14.8-1.37 14.42 4.47-.4 5.85-14.42 18.38-14.42 18.38z"/>
+            </g>
+            <g>
+                <text transform="translate(190 194)">
+                    <tspan x=".3" y="35">8888888</tspan>
+                </text>
+            </g>
+        </g>
+    </g>
+    <g class="HomeHeroBg-three" fill="none" fill-rule="evenodd" opacity="0">
+        <g class="HomeHeroBg-boxes">
+            <g>
+                <rect class="box line wide" x="287" y="42" width="300" height="620" rx="40"/>
+            </g>
+            <g>
+                <path class="box" d="M509.5 64.5h57v57h-57z"/>
+            </g>
+            <g transform="translate(297 63)">
+                <path class="box" d="M1.5 68.5h277v147H1.5z"/>
+            </g>
+            <g>
+                <path class="box" d="M306.5 64.5h197v57h-197z"/>
+                <path class="box" d="M318.5 575.5h112v77h-112z"/>
+                <path class="box" d="M443.5 484.5h112v77h-112z"/>
+                <path class="box" d="M443.5 575.5h112v77h-112z"/>
+                <path class="box" d="M318.5 484.5h112v77h-112z"/>
+            </g>
+        </g>
+        <g class="HomeHeroBg-content">
+            <g>
+                <path class="line med" d="M525.6 82.69h24.35M525.6 92.55h24.35M525.6 103.31h24.35"/>
+            </g>
+            <g>
+                <path class="line thick" d="M316.7 365.5H468M316.7 456.5H558M316.7 336.5H468M316.7 397.5H558M316.7 426.5H558M316.7 307.5H468"/>
+                <path class="line med" d="M485 305h67v65h-67z"/>
+            </g>
+        </g>
+        <g class="HomeHeroBg-labels">
+            <g>
+                <path class="arrow" d="M254.42 97.11l-7.74-7.15-1.27 18.96 10.56-10.11c-22.97.4-37.25-4.95-45.97-12.98"/>
+                <text transform="rotate(19 -26.02 478.11)">
+                    <tspan x="4.47" y="41.37">logo</tspan>
+                </text>
+            </g>
+            <g>
+                <path class="arrow" d="M619.6 394.08l5.46-9 6.44 17.88-12.93-6.81c22.18-5.95 34.44-15.02 40.6-25.15"/>
+                <text transform="rotate(19 -540.46 2028.08)">
+                    <tspan x="25.27" y="52.53">body text</tspan>
+                </text>
+            </g>
+            <g>
+                <path class="arrow" d="M626.85 563.31l5.93 8.71 5.5-18.19-12.56 7.48c22.46 4.77 35.18 13.2 41.87 22.98"/>
+                <text transform="rotate(-18 2113.94 -1619.35)">
+                    <tspan x="9" y="68.73">portfolio</tspan>
+                    <tspan x="26.62" y="103.73">items</tspan>
+                </text>
+            </g>
+            <g>
+                <path class="arrow" d="M263.8 209.3l-9.07 5.39 2.71-18.82 8.22 12.09c-22.38-5.17-37.46-2.9-47.66 3.14"/>
+                <text transform="rotate(30 -230.73 416.98)">
+                    <tspan x="18.95" y="46.81">hero</tspan>
+                    <tspan x="13.08" y="81.81">image</tspan>
+                </text>
+            </g>
+            <g>
+                <path class="arrow" d="M617.15 86.35l7.22-7.67 2.59 18.82-11.24-9.35c22.94-1.2 36.81-7.53 44.95-16.15"/>
+                <text transform="rotate(19 297.11 1902.49)">
+                    <tspan x="69.15" y="46.44">menu</tspan>
+                    <tspan x="59.16" y="81.44">button</tspan>
+                </text>
+            </g>
+        </g>
+    </g>
+</svg>
--- resources/views/main/portfolio/list_hero.blade.php
+++ resources/views/main/portfolio/list_hero.blade.php
@@ -1,20 +1,40 @@
 @if(isset($items) && count($items))
-    <div class="PortfolioListHero">
-        <div class="PortfolioListHero-heading">
-            <a href="{{ route('portfolio.index') }}">
-                <h3 class="PortfolioListHero-title">
-                    Some websites I have made
-                </h3>
-            </a>
-        </div>
-    	@foreach($items as $key => $item)
-    	    <div class="PortfolioListHero-item PortfolioListHero-item--{{$loop->iteration}}">
-                @include('main.portfolio.excerpt', ['item' => $item, 'slug' => $key])
-    	    </div>
-    	@endforeach
-        <div class="PortfolioListHero-more">
-            <a class="PortfolioListHero-more-btn" href="{{ route('portfolio.index') }}">
-                View more</a>
-        </div>
+<div class="PortfolioListHero">
+    {{-- <div class="PortfolioListHero-heading">
+            <h3 class="PortfolioListHero-title">
+                Some things I have made
+            </h3>
+        </div> --}}
+    <div class="PortfolioListHero-intro PortfolioListHero-intro--1">
+        <p>
+            &hellip;like this thing&hellip;
+        </p>
+        <svg class="PortfolioListHero-arrow" width="28" height="70" viewBox="0 0 28 70" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.42 1.92c5.08 44.78 2.27 65.7 2.27 65.7l13.34-21.38-24.22-.02 10.26 20.72" stroke="#979797" stroke-width="3" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
     </div>
+    @foreach($items as $key => $item)
+    <div class="PortfolioListHero-item PortfolioListHero-item--{{$loop->iteration}}">
+        @include('main.portfolio.excerpt', ['item' => $item, 'slug' => $key])
+    </div>
+    @endforeach
+    <div class="PortfolioListHero-intro PortfolioListHero-intro--2">
+        <svg class="PortfolioListHero-arrow PortfolioListHero-arrow--left" width="160" height="25" viewBox="0 0 160 25" xmlns="http://www.w3.org/2000/svg">
+            <path d="M80 23.13C80-6.47 2.23 12.94 2.23 12.94L21.07 1.96l2.59 16.4-20.9-4.95" stroke="#979797" stroke-width="3" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        <p>
+            &hellip;and these bits
+        </p>
+        <svg class="PortfolioListHero-arrow" width="25" height="70" viewBox="0 0 25 70" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.28 2.38C4.63 28.64 17.1 67.93 17.1 67.93L1.47 48.54l21.16-4.43-5.08 22.93" stroke="#979797" stroke-width="3" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    </div>
+    <div class="PortfolioListHero-more">
+        <p>View more on
+            <a class="PortfolioListHero-more-btn" href="{{ route('portfolio.index') }}">my professional portfolio</a>
+            or checkout some
+            <a class="PortfolioListHero-more-btn" href="{{ route('portfolio.index') }}">random Projects</a>
+        </p>
+    </div>
+</div>
 @endif
